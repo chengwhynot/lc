d89e91113dcf26efb09578c729d51b2989ee6e7d
zhangcheng17 <zhangcheng17@longfor.com>
2022-06-21 09:15:14 +0800

    twosum

--- 1.两数之和/test_twosum.py
+++ 1.两数之和/test_twosum.py
@@ -1,4 +1,8 @@
+from twosum import solution
 
 def test_twoSum_01():
-    
-    assert True
+    data = [1,2,3,2,5]
+    actural = solution.twoSum(data, 5)
+    expected1 = [2,3]
+    expected2 = [1,2]
+    assert actural == expected1 or actural == expected2
